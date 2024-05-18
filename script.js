2b8ff18b73058adc53f43a2a7ceb4eafec03b305
aki <139993309+z3ro-c0nfig@users.noreply.github.com>
2024-05-18 15:57:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,12 @@ console.log(`_______  ___   _  ___   _______        ______   _______  __   __
 `)
 console.log("==================================================================")
 
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+		return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+		return false;
+    }
+});
+
 document.addEventListener('contextmenu', event => event.preventDefault()); // Prevent right click
